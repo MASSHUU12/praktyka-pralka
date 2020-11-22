f033df988b5efa5b62049ee76cfcb854d8a097fa
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2020-11-22 21:09:29 +0100

    The icon of the circle under the banner changes with the advertisement

--- views/index.php
+++ views/index.php
@@ -3,19 +3,11 @@
     <main>
     <section class="main-banner">
         <img class="main-banner-img" id="slider" src="app/public/img/1.jpg" />
-<<<<<<< Updated upstream
-    </section>
-    <section>
-        <div class="main-banner-button" onclick="setSlide(1)"></div>
-        <div class="main-banner-button" onclick="setSlide(2)"></div>
-        <div class="main-banner-button" onclick="setSlide(3)"></div>
-=======
         <div class="main-banner-buttons">
             <i class="fas fa-circle main-banner-button" id="main-banner-button1" onclick="setSlide(1)"></i>
             <i class="fas fa-circle main-banner-button" id="main-banner-button2" onclick="setSlide(2)"></i>
             <i class="fas fa-circle main-banner-button" id="main-banner-button3" onclick="setSlide(3)"></i>
         </div>  
->>>>>>> Stashed changes
     </section>
         <div class="main-container">
             <div class="container-offers">
