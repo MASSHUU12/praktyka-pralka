3847c64d1bfff485c131975cae1904b832770533
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2020-11-24 16:12:00 +0100

    Update header.php

--- views/inc/header.php
+++ views/inc/header.php
@@ -37,7 +37,7 @@
             <i class="fas fa-shopping-cart fa-lg"></i>
         </div>
         <div class="header-hamburger-dropdown">
-            <i onclick="headerHamburger()" class="header-hamburger-dropdown-button">dropdown</i>
+            <i onclick="headerHamburger()" class="header-hamburger-dropdown-button"></i>
             <div id="header-hamburger-dropdown-id" class="header-hamburger-dropdown-content">
                 <p>Profile</p>
                 <p>Orders</p>
