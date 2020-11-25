bb1201f847b0113270f18800825e936092fe3d50
jacqouese <jakub.czaja003@gmail.com>
2020-11-25 20:44:49 +0100

    login changes

--- controllers/login.php
+++ controllers/login.php
@@ -4,18 +4,23 @@ class Login extends SignupLoginModel {
     
     public function getUser($username, $password) {
        $results = $this->checkUser($username);
-        $remotePassword = $results[0]['passwordUsers'];
-        $passwordCheck = password_verify($password, $remotePassword);
-        if ($passwordCheck == false) 
-            echo 'Wrong password';
-        else if ($passwordCheck == true) {
-            $_SESSION['userId'] = $results[0]['Id'];
-            $_SESSION['username'] = $results[0]['usernameUsers'];
-            header("Location: /?login=success");
-        }
-        else 
-            echo 'Some error occured. Please try again';
+       if (!empty($results)) {
+            $remotePassword = $results[0]['passwordUsers'];
+            $passwordCheck = password_verify($password, $remotePassword);
+            if ($passwordCheck == false) 
+                echo 'Wrong password';
+            else if ($passwordCheck == true) {
+                $_SESSION['userId'] = $results[0]['Id'];
+                $_SESSION['username'] = $results[0]['usernameUsers'];
+                header("Location: /?login=success");
+            }
+            else 
+                echo 'Some error occured. Please try again';
               
+        }
+        else
+            echo 'Konto nie istnieje';
     }
+        
 
 }
